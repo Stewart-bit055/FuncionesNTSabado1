93041b4e0f954749359aaba70de4f85b03360382
Stewart-bit055 <rodriguezstewart16@gmail.com>
2026-02-28 10:17:14 -0500

    feat se agrega función calculadora de promedios

---
+++ funcionCuatro.py
@@ -0,0 +1,3 @@
+def evaluar_bicicleta(eficiencia,velocidad,parecido):
+    evaluacion=(eficiencia*0.4)+(velocidad*0.4)+(parecido*0.2)
+    return evaluacion
---
+++ funcionTres.py
@@ -0,0 +1,3 @@
+#crear una funcion que calcula el promedio de una lista de notas
+def calcular_promedio_notas(notas):     
+    return(sum(notas)/len(notas))
